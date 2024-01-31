61466a46fe75a6ab8b992982caf3dc17ef04be10
tjommyboy Wiersdalen <tommywiersdalen@gmail.com>
2024-01-31 11:15:19 +0100

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+	return (
+		<>
+			<div className="flex items-center m-auto w-1/2">
+				<div>
+					<h1 className="font-bold text-3xl">NEWS score calculator</h1>
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default App;
